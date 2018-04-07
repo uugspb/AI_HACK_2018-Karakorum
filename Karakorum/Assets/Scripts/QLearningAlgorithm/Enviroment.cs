7d8f6c285eae056941b74c2bd27c297d3c8b7aae
Alex [Byton] <alex.tyk@yandex.ru>
2018-04-08 00:40:46 +0300

    Update after concert

---
+++ Karakorum/Assets/Scripts/QLearningAlgorithm/Enviroment.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Karoku.QLearning
+{
+    public class Enviroment
+    {
+        public Enviroment(
+            int width,
+            int height,
+            float quadWidth,
+            float quadHeights,
+            Vector3 mapOffset)
+        {
+            map = new int[Width = width, Height = height];
+            MapOffset = mapOffset;
+            QuadWidth = quadWidth;
+            QuadHeights = quadHeights;
+
+        }
+
+
+        public readonly int Width;
+        public readonly int Height;
+        public readonly float QuadWidth;
+        public readonly float QuadHeights;
+        public readonly Vector3 MapOffset;
+
+        private readonly int[,] map;
+
+
+        public int this[int x, int y]
+        {
+            get { return map[x, y]; }
+            set { map[x, y] = value; }
+        }
+
+
+        public Vector3 CoordToPoint(int x, int y)
+        {
+            return new Vector3(QuadWidth * (x + .5f), QuadHeights * (y + .5f)) + MapOffset;
+        }
+
+        public int[] PointToCoord(Vector3 position)
+        {
+            position -= MapOffset;
+            int[] res = new int[2];
+            res[0] = Mathf.CeilToInt(position.x / Width);
+            res[1] = Mathf.CeilToInt(position.z / Height);
+            return res;
+        }
+    }
+}
